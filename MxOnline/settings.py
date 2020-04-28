e575059ec54edeb5f87ca3c6d5edc6fa67dbbc76
wanglong-github <59468022+wanglong-github@users.noreply.github.com>
2020-04-28 21:13:47 +0800

    xadmin的配置

--- MxOnline/settings.py
+++ MxOnline/settings.py
@@ -38,7 +38,10 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'apps.users.apps.UsersConfig',
-    'apps.courses.apps.CoursesConfig'
+    'apps.courses.apps.CoursesConfig',
+    'crispy_forms',
+    'xadmin.apps.XAdminConfig',
+
 ]
 
 MIDDLEWARE = [
--- apps/users/admin.py
+++ apps/users/admin.py
@@ -1,7 +1,13 @@
 from django.contrib import admin
+#
+# # Register your models here.
+# from apps.users.models import UserProfile
+# class UserProfileAdmin(admin.ModelAdmin):
+#     pass
+# admin.site.register(UserProfile,UserProfileAdmin)
 
-# Register your models here.
+# 因为用户表相对说都是系统中存在的，django内置了一个UserAdmin对显示用户信息做了优化
 from apps.users.models import UserProfile
-class UserProfileAdmin(admin.ModelAdmin):
-    pass
-admin.site.register(UserProfile,UserProfileAdmin)
+from django.contrib.auth.admin import UserAdmin
+
+admin.site.register(UserProfile, UserAdmin)
