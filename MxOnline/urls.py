450df9793dde6c4083bda8e5144181d20b69e40e
wanglong-github <59468022+wanglong-github@users.noreply.github.com>
2020-04-30 20:31:00 +0800

    django项目登录

--- MxOnline/urls.py
+++ MxOnline/urls.py
@@ -2,7 +2,15 @@
 from django.contrib import admin
 from django.urls import path
 import xadmin
+# from apps.users import views
+from django.views.generic import TemplateView
+from apps.users.views import LoginView
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('xadmin/', xadmin.site.urls),
+    path('', TemplateView.as_view(template_name='index.html'),name='index'),
+    path('login/', LoginView.as_view(),name='login'),
+
 ]
