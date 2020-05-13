140af1de4466e07dd21ea2c2971348092583ad9f
wanglong-github <59468022+wanglong-github@users.noreply.github.com>
2020-05-13 16:14:21 +0800

    课程收藏

--- apps/courses/views.py
+++ apps/courses/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.views.generic.base import View
 from apps.courses.models import Course
 from pure_pagination import Paginator, EmptyPage, PageNotAnInteger
-
+from apps.operations.models import UserFavorite
 # Create your views here.
 class CourseListView(View):
     def get(self, request, *args, **kwargs):
@@ -40,9 +40,18 @@ class CourseDetailView(View):
         # 点击到课程的详情就记录一次点击数
         course.click_nums+=1
         course.save()
-
+        # 获取收藏状态
+        has_fav_course = False
+        has_fav_org = False
+        if request.user.is_authenticated:
+            # 查询用户是否收藏了该课程和机构 fav_type=1证明是课程收藏，如果有，证明用户收藏了这个课
+            if UserFavorite.objects.filter(user=request.user, fav_id=course.id, fav_type=1):
+                has_fav_course = True
+            if UserFavorite.objects.filter(user=request.user, fav_id=course.id, fav_type=2):
+                has_fav_org = True
         return render(request, 'course-detail.html',
                       {"course":course,
-
+                       "has_fav_course": has_fav_course,
+                       "has_fav_org": has_fav_org,
                        },
                     )
